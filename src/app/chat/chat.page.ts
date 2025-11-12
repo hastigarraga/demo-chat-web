d8fbbf73de980c6b6ea2e836f1e0f8a0a97d9199
hastigarraga <hastigarraga@hotmail.com>
2025-11-12 13:38:49 -0300

    update chats

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit, ViewChild, ElementRef, HostListener } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
